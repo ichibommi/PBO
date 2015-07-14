b5568dd213848202bcf86226f0c57b41545854c9
Yuan Lukito <yuanlukito@ti.ukdw.ac.id>
2015-07-14 15:00:23 +0700

    Demo Pertemuan 01
    Menggunakan command prompt untuk kompilasi dan menjalankan program yang
    ditulis dengan java

---
+++ pertemuan-01/demo/01-commandline-demo/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+	Demo PBO 01
+	Menjalankan program Java di Command Line
+
+	dibuat oleh yuanlukito[at]ti.ukdw.ac.id
+*/
+	
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ pertemuan-01/demo/01-commandline-demo/Introduction.java
@@ -0,0 +1,20 @@
+/*
+	Demo PBO 01
+	Menjalankan program Java di Command Line
+
+	dibuat oleh yuanlukito[at]ti.ukdw.ac.id
+*/
+
+public class Introduction {
+	public static void main(String[] args) {
+		System.out.println("Halo, saya adalah program yang ditulis");
+		System.out.println("dengan bahasa pemrograman Java");
+		System.out.println("Lihat, saya bisa menggambar:");
+		System.out.println("==========================================");
+		System.out.println("=====  :      |   |  | /  |=\\   |  |  |");
+		System.out.println("  |    |      |   |  |/   |  \\  |  |  |");
+		System.out.println("  |    |      |   |  |\\   |  /  |  |  |");
+		System.out.println("  |    |      |===|  | \\  |=/    \\/ \\/ ");
+		System.out.println("==========================================");
+	}
+}
