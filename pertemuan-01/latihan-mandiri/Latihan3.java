4a0fe17bd5f85993e1cd0035d8e557ffa91acfd4
Yuan Lukito <yuanlukito@ti.ukdw.ac.id>
2015-07-22 10:33:34 +0700

    Update Latihan3 dari Latihan Mandiri 01
    Menghapus kode program yang tidak diperlukan

--- pertemuan-01/latihan-mandiri/Latihan3.java
+++ pertemuan-01/latihan-mandiri/Latihan3.java
@@ -9,12 +9,12 @@
 public class Latihan3{
 	public static void main(String[] args) {
 		//definisikan object bernama star dari class Stars
-		Stars star = new Stars();
+		
 		//panggil method print dari star
-		star.print();
+		
 		//ubah property lineCount menjadi 15
-		star.setLineCount(15);
+		
 		//panggil method print sekali lagi
-		star.print();
+		
 	}
 }
