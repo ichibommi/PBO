32c5b467da3972f651c905e3e96017cd29ff0fb5
Yuan Lukito <yuanlukito@ti.ukdw.ac.id>
2015-07-23 10:51:54 +0700

    Demo 02: Netbeans
    Tambahan demo program menggunakan Netbeans

---
+++ pertemuan-01/demo/02-netbeans-demo/JavaApplication/src/javaapplication/JavaApplication.java
@@ -0,0 +1,20 @@
+/*
+ */
+package javaapplication;
+
+/**
+ *
+ * @author Yuan Lukito
+ */
+public class JavaApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Halo, program ini dibuat dengan bantuan Netbeans");
+        System.out.println("Ini muncul di baris kedua");
+    }
+    
+}
